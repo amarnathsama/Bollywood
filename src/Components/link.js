f9195ad33cc585fe9df3b3c114baa13cc9da764a
amarnathsama <amar22111999@gmail.com>
2021-05-11 15:08:22 +0530

    second commit

--- src/Components/link.js
+++ src/Components/link.js
@@ -1,3 +1,3 @@
 export const offlineUrl = "http://localhost:5000";
 export const deployedUrl = "https://bollywoodbackend.herokuapp.com";
-export const inUseUrl = deployedUrl;
+export const inUseUrl = offlineUrl;
