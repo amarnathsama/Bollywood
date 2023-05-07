e17639829337ebbad345d971a20f4fac9b4a4a05
amarnathsama <63007641+amarnathsama@users.noreply.github.com>
2023-05-07 21:47:51 +0530

    Update link.js

--- src/Components/link.js
+++ src/Components/link.js
@@ -1,3 +1,3 @@
 export const offlineUrl = "http://localhost:5000";
-export const deployedUrl = "https://bollywoodbackend.herokuapp.com";
+export const deployedUrl = "https://spectacular-torrone-e904ae.netlify.app/";
 export const inUseUrl = deployedUrl;
